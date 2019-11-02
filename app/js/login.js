3ffd130d4b9aee67664f6ff834d0986cfc246169
Pedro Almeida <pedrohenriquealmeidacosta@gmail.com>
2019-11-02 20:33:51 -0300

    login implemented

--- app/js/login.js
+++ app/js/login.js
@@ -1,24 +1,29 @@
-let url = 'http://localhost:8080/users/login';//olhar qual a url para passar para o backend, não sei se é esta mesmo
+const BASE_URL = 'http://localhost:8080/debpay/';
+const HTTP_OK = 200;
+const HTTP_UNAUTHORIZED = 401;
 
 function onLogin() {
-    let email = document.getElementById('login-email-field');
-    let password = document.getElementById('login-password-field');
-    let json = { "login": email, "password": password};
-    let request = new Request(url);   
+    let email = document.getElementById('login-email-field').value;
+    let password = document.getElementById('login-password-field').value;
+    let body = { "login": email, "password": password};
     
-    request.method = 'POST';
-    request.body = json;
-    request.mode = 'cors';
-    
-    fetch(request).then((response) => {
-        if (response.status == 200) {
-            sessionStorage.setItem('id', this.getResponseHeader('id'));
+    fetch(BASE_URL + "user/login",{
+        mode:"cors",
+        method: 'POST',
+        body: JSON.stringify(body)
+    }).then((response) => {
+        if (response.status == HTTP_OK) {
+            response.json().then((data) => {
+                sessionStorage.setItem('id', data)
+            });
+
             window.location.href = "main.html";
-        }else if(response.status == 401){
+        }else if(response.status == HTTP_UNAUTHORIZED){
+            // TODO create a better solution. Maybe modals ?
             alert('E-mail ou senha inválido(s)');
-        }
-    }).catch((e) =>{
-        console.log("Fetch error: "+ e);
+        } // TODO treat server errors
+    }).catch((e) => {
+        console.log("Fetch error: " + e);
     });
 }
 
